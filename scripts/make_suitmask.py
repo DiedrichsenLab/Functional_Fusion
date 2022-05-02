a30574e610dcb5586a660f0159259d507f47e4e2
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2022-05-02 18:43:13 -0400

    Make suit2mm

--- scripts/make_suitmask.py
+++ scripts/make_suitmask.py
@@ -3,34 +3,23 @@ import numpy as np
 import nibabel as nb
 from atlas_map import AtlasVolumetric, AtlasMapDeform, get_data
 from dataset import DataSetMDTB
+import SUITPy as suit
 
 base_dir = '/Volumes/diedrichsen_data$/data/FunctionalFusion'
 data_dir = base_dir + '/MDTB'
 atlas_dir = base_dir + '/Atlases'
 hcp_dir = base_dir + '/HCP'
 
-def explore_cifti():
-    dir = hcp_dir + '/derivatives/100307/estimates'
-    A=nb.load(dir+'/sub-100307_ses-01_task-rest_space-fsLR32k_run-01_bold.nii')
-    ser = A.header.get_axis(0)
-    bmf = A.header.get_axis(1)
-
-    for idx, (nam,slc,bm) in enumerate(bmf.iter_structures()):
-        print(idx,str(nam),slc)
-    pass
-    bmcl = bmf[bmf.name == 'CIFTI_STRUCTURE_CEREBELLUM_LEFT']
-    bmcr = bmf[bmf.name == 'CIFTI_STRUCTURE_CEREBELLUM_RIGHT']
-    bmc= bmcl + bmcr
-    ijk = bmc.voxel
-    X = np.zeros((90,50,50))
-    X[ijk[:,0],ijk[:,1],ijk[:,2]] = 1
-    N = nb.Nifti1Image(X,bmc.affine)
-    nb.save(N,atlas_dir + '/tpl-MNI152AsymC_res-2' + '/tpl-MNI152AsymC_res-2_gmcmask.nii')
-    pass
-
+def reslice_SUIT():
+    adir = atlas_dir +'/tpl-MNI152NLin6AsymC'
+    img_name = adir + '/tpl-MNI152NLin6AsymC_res-2_gmcmask.nii'
+    def_name = adir + '/tpl-MNI152NLin6AsymC_space-SUIT_xfm.nii'
+    a = suit.reslice.reslice_image(img_name,def_name,interp=1)
+    nb.save(a,atlas_dir + '/tpl-SUIT/tpl-SUIT_res-2_gmcmask.nii')
+    pass 
 
 if __name__ == "__main__":
-    explore_cifti()
+    reslice_SUIT()
 
 
     # T= pd.read_csv(data_dir + '/participants.tsv',delimiter='\t')
