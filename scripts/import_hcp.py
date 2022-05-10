03113d2c4362d13c4dd7a19eee8171051bb88704
lshahsha <lshahsha@uwo.ca>
2022-05-10 17:36:30 -0400

    modified import of HCP with data put into separate ses folder
    task-rest was deleted from the name of the file
    LR is always run01 and RL is run02
    We don't need a folder called estimates as this is a resting state data and a folder called func makes more sense.

--- scripts/import_hcp.py
+++ scripts/import_hcp.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Script for importing the HCP data set to general format.
-
 Created on 4/25/2022 at 12:18 PM
 Author: dzhi
 """
@@ -10,11 +9,10 @@ import pandas as pd
 import shutil
 from pathlib import Path
 import os
-base_dir = 'Y:/data'
+# base_dir = 'Y:/data'
+base_dir = '/Volumes/diedrichsen_data$/data'
 orig_dir = os.path.join(base_dir, 'HCP_UR100_rfMRI')
 target_dir = os.path.join(base_dir, 'FunctionalFusion/HCP')
-
-
 def import_func_resting(source_dir, dest_dir, participant_id):
     """Imports the HCP preprocessed resting state files
        into a BIDS/derivative structure
@@ -23,6 +21,7 @@ def import_func_resting(source_dir, dest_dir, participant_id):
         dest_dir (str): destination directory
         participant_id (str): ID of participant
     """
+<<<<<<< Updated upstream
    
     src=[]
     dest =[]
@@ -47,10 +46,44 @@ def import_func_resting(source_dir, dest_dir, participant_id):
 
 
 if __name__ == "__main__":
+=======
+    
+    for ss in [1, 2]:
+         src=[]
+         dest =[]
+         # add a folder for session
+         dest_sess_dir = dest_dir + f"/ses-{ss:02}"
+         print(dest_sess_dir)
+         # Make the destination directory
+         Path(dest_sess_dir).mkdir(parents=True, exist_ok=True)
+         # move data into the corresponding session folder
+         src.append(f'/rfMRI_REST{ss}_LR/rfMRI_REST{ss}_LR_Atlas_hp2000_clean.dtseries.nii')
+         dest.append(f'/sub-{participant_id}_ses-{ss:02}_space-fsLR32k_run-01.dtseries.nii')
+         src.append(f'/rfMRI_REST{ss}_RL/rfMRI_REST{ss}_RL_Atlas_hp2000_clean.dtseries.nii')
+         dest.append(f'/sub-{participant_id}_ses-{ss:02}_space-fsLR32k_run-02.dtseries.nii')
+         for i in range(len(src)):
+             try:
+                 print(src[i])
+                 print(dest[i])
+                 shutil.copyfile(source_dir+'/MNINonLinear/Results'+src[i], dest_sess_dir+dest[i])
+             except:
+                 print('skipping ' + src[i])
+
+def main():
+>>>>>>> Stashed changes
     T = pd.read_csv(target_dir + '/participants.tsv', delimiter='\t')
     for s in T.participant_id:
+        print(f"-Doing subject {s}")
         # old_id = s.replace('sub-','s',1)
         dir1 = os.path.join(orig_dir, str(s))
-        dir2 = os.path.join(target_dir, 'derivatives/%s/estimates' % str(s))
+        dir2 = os.path.join(target_dir, 'derivatives/%s/func' % str(s))
         import_func_resting(dir1, dir2, str(s))
 
+if __name__ == "__main__":
+    T = pd.read_csv(target_dir + '/participants.tsv', delimiter='\t')
+    for s in T.participant_id:
+        print(f"-Doing subject {s}")
+        # old_id = s.replace('sub-','s',1)
+        dir1 = os.path.join(orig_dir, str(s))
+        dir2 = os.path.join(target_dir, 'derivatives/%s/func' % str(s))
+        import_func_resting(dir1, dir2, str(s))
