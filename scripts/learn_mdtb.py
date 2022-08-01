cc200617aeaf0afede25e7b7c78dab2a2af52f9b
dzhi1993 <r930710@gmail.com>
2022-08-01 16:32:50 -0400

    Add fusion routine for HCP and MDTB.

--- scripts/learn_mdtb.py
+++ scripts/learn_mdtb.py
@@ -18,6 +18,7 @@ import Functional_Fusion.matrix as matrix
 import matplotlib.pyplot as plt
 import seaborn as sb
 import sys
+from get_hcp_data import get_hcp_data
 
 base_dir = '/Volumes/diedrichsen_data$/data/FunctionalFusion'
 if not Path(base_dir).exists():
@@ -78,62 +79,40 @@ def get_mdtb_data(ses_id='ses-s1', type='CondSes'):
 
     return Data, Xcond, D
 
-def get_hcp_data(tessel=162, ses_id=['ses-01'], range=None, save=False):
-    """Get the HCP resting-state connnectivity profile
-       The running time of this function is very slow (~1m per subject/sess)
-    Args:
-        tessel: the cortical map used to generate connectivity
-        ses_id: session id
-        range: the index of subject among unrelated 100 dataset
-               default - None, which means get all participants
-    Returns:
-        the HCP rs-FC, shape (n_subj, N, P) - N is based on tessel
-    """
-    suit_atlas = am.AtlasVolumetric('SUIT', mask_img=mask)
-    deform = base_dir + '/Atlases/tpl-MNI152NLin6AsymC/tpl-MNI152NLin6AsymC_space-SUIT_xfm.nii'
-    MNI_mask = base_dir + '/Atlases/tpl-MNI152NLin6AsymC/tpl-MNI152AsymC_res-2_gmcmask2.nii'
+def get_hcp_data(res=162, index=range(0, 100), runs=[0,1,2,3], refix=False):
+    # Make the atlas object
+    mask = atlas_dir + '/tpl-SUIT/tpl-SUIT_res-3_gmcmask.nii'
+    suit_atlas = am.AtlasVolumetric('cerebellum', mask_img=mask)
 
-    # get the tessellation file
-    tessel_dir = atlas_dir + '/tpl-fs32k'
-    # get the gifti file for the label
-    gii_labels = [nb.load(tessel_dir + f'/Icosahedron-{tessel}.32k.L.label.gii'),
-                  nb.load(tessel_dir + f'/Icosahedron-{tessel}.32k.R.label.gii')]
+    # initialize the data set object
+    hcp_dataset = DataSetHcpResting(hcp_dir)
 
-    # get the gifti of the mask
-    gii_mask = [nb.load(atlas_dir + f'/tpl-fs32k/tpl-fs32k_hemi-L_mask.label.gii'),
-                nb.load(atlas_dir + f'/tpl-fs32k/tpl-fs32k_hemi-R_mask.label.gii')]
+    # Get the deformation map from MNI to SUIT
+    mni_atlas = atlas_dir + '/tpl-MNI152NLin6AsymC'
+    deform = mni_atlas + '/tpl-MNI152NLin6AsymC_space-SUIT_xfm.nii'
+    mask = mni_atlas + '/tpl-MNI152NLin6AsymC_res-2_gmcmask.nii'
+    atlas_map = am.AtlasMapDeform(hcp_dataset, suit_atlas, 'group', deform, mask)
+    atlas_map.build(smooth=2.0)
+
+    # Get the parcelation
+    hem_name = ['cortex_left', 'cortex_right']
+    surf_parcel = []
+    for i, h in enumerate(['L', 'R']):
+        dir = atlas_dir + '/tpl-fs32k'
+        gifti = dir + f'/Icosahedron-{res}.32k.{h}.label.gii'
+        surf_parcel.append(am.AtlasSurfaceParcel(hem_name[i], gifti))
 
     T = hcp_dataset.get_participants()
-
-    if range is not None:
-        id = T.participant_id.values[range]
-    else:
-        id = T.participant_id.values
-
-    output = []
-    for s in id:
-        con_fp = []  # connectivity finger print list
-        for sess in ses_id:
-            # create a mapping based on atlas deformation
-            atlas_map = am.AtlasMapDeform(hcp_dataset, suit_atlas, s, deform, MNI_mask)
-            # print(f"building atlas map")
-            atlas_map.build(smooth=2.0)  # smoothing level?
-
-            # get the connectivity matrix and put it in a list
-            data = hcp_dataset.get_data(s, [atlas_map], gii_labels, gii_mask, sess)
-
-            if save:
-                # save as np.ndarry as .csv under the same folder in fusion
-                print(f'Saving sub-{s}, {sess} rs-FC')
-                target_dir = hcp_dir + f'/derivatives/{s}/func/{sess}'
-                pd.DataFrame(data.T).to_csv(target_dir+f'/sub-{s}_{sess}_tessel-{tessel}_conn.csv')
-
-            con_fp.append(pt.tensor(data.T))
-
-        sub_data = pt.vstack(con_fp)
-        output.append(sub_data)
-
-    return pt.stack(output)  # output is torch tensor
+    data = []
+    for s in T.participant_id[index]:
+        print(f'Extract {s}')
+        coef = hcp_dataset.get_cereb_connectivity(s, atlas_map, surf_parcel,
+                                                  runs=runs, refix=refix)
+        # Average or Contatenate across runs? Not sure.
+        # For computation efficiency, use average
+        coef = np.nanmean(coef, axis=0)
+        data.append(coef)
+    return np.asarray(data)
 
 def get_hcp_data_from_csv(tessel=162, ses_id=['ses-01'], range=None):
 
@@ -266,8 +245,22 @@ def learn_single(ses_id='ses-s1', max_iter=100, fit_arr=False):
 
     pass
 
-def learn_runs(K=10, max_iter=100, run_test=np.arange(58, 122),
-               runs=np.arange(1, 17), sub=None, do_plot=True):
+def learn_runs(K=10, max_iter=100, runs=np.arange(1, 17), sub=None, do_plot=True):
+    """Run this function to replicate the figure 5 in the nips paper
+    Args:
+        K: the number of parcels
+        max_iter: maximum iterations for EM learning
+        runs: the number of runs data to use (accumulative)
+        sub: None
+        do_plot: plot if True
+    Returns:
+        group_baseline: The cosine error for the group baseline
+        lower_bound: The noise floor
+        cos_em: The cosine error for the data only
+        cos_complete: The cosine error for data+prior
+        uhat_em_all: The exepcation obtained by data only
+        uhat_complete_all: The expecation obtained by data+prior
+    """
     Data_1, Xdesign_1, D1 = get_mdtb_data(ses_id='ses-s1', type='CondRun')
     Data_2, Xdesign_2, D2 = get_mdtb_data(ses_id='ses-s2', type='CondRun')
     Xdesign = pt.tensor(block_diag(Xdesign_1, Xdesign_2))
@@ -377,9 +370,235 @@ def learn_runs(K=10, max_iter=100, run_test=np.arange(58, 122),
 
     return group_baseline, lower_bound, cos_em, cos_complete, uhat_em_all, uhat_complete_all
 
+def learn_hcp_mdtb(K=10, max_iter=100, runs=np.arange(1, 17), sub=None, do_plot=True):
+    """Run this function to replicate the figure 5 in the nips paper
+    Args:
+        K: the number of parcels
+        max_iter: maximum iterations for EM learning
+        runs: the number of runs data to use (accumulative)
+        sub: None
+        do_plot: plot if True
+    Returns:
+        group_baseline: The cosine error for the group baseline
+        lower_bound: The noise floor
+        cos_em: The cosine error for the data only
+        cos_complete: The cosine error for data+prior
+        uhat_em_all: The exepcation obtained by data only
+        uhat_complete_all: The expecation obtained by data+prior
+    """
+    Data_1, Xdesign_1, D1 = get_mdtb_data(ses_id='ses-s1', type='CondRun')
+    Data_2, Xdesign_2, D2 = get_mdtb_data(ses_id='ses-s2', type='CondRun')
+    Xdesign = pt.tensor(block_diag(Xdesign_1, Xdesign_2))
+    Data = np.concatenate((Data_1, Data_2), axis=1)
+    D1['sess'] = 1  # Add a column of sess = 1
+    D2['sess'] = 2  # Add a column of sess = 2
+
+    Data_hcp = get_hcp_data(res=162, index=range(0, 80), runs=[0,1,2,3])
+    # Make arrangement model and initialize the prior from the MDTB map
+    P = Data.shape[2]
+    prior_w = 7.0  # Weight of prior
+    mdtb_parcel, mdtb_colors = get_mdtb_parcel(do_plot=False)
+    logpi = ar.expand_mn(mdtb_parcel.reshape(1, P) - 1, K)
+    logpi = logpi.squeeze() * prior_w
+    # Set parcel 0 to unassigned
+    logpi[:, mdtb_parcel == 0] = 0
+
+    # Train on sc 1 data
+    run_idx_1 = pt.tensor(D1[['sess', 'run']].values)
+    run_idx_1 = pt.unique(run_idx_1[:, 0] * 100 + run_idx_1[:, 1], return_inverse=True)[1]
+    ar_model = ar.ArrangeIndependent(K=K, P=P, spatial_specific=True,
+                                         remove_redundancy=False)
+    em_model = em.MixVMF(K=K, N=40, P=P, X=Xdesign_1, uniform_kappa=True)
+    em_model.initialize(Data_1, D=run_idx_1)
+    em_model_2 = em.MixVMF(K=K, N=Data_hcp.shape[1], P=P, uniform_kappa=True)
+    em_model_2.initialize(Data_hcp)
+
+    # Initilize parameters from group prior and train the m odel
+    mdtb_prior = logpi.softmax(dim=0)
+    M = fm.FullMultiModel(ar_model, [em_model, em_model_2])
+    M.pre_train([Data_1, Data_hcp], iter=10, prior=mdtb_prior)
+    M, ll, theta, U_hat = M.fit_em(Y=[Data_1, Data_hcp], iter=max_iter, tol=0.00001,
+                                      fit_arrangement=True)
+    plt.plot(ll, color='b')
+    plt.show()
+
+    ### fig a: Plot group prior
+    _plot_maps(pt.argmax(M.arrange.logpi, dim=0) + 1, color=True, render_type='matplotlib',
+               save='group_prior.pdf')
+    prior = pt.softmax(M.arrange.logpi, dim=0).unsqueeze(0).repeat(Data.shape[0], 1, 1)
+
+    # train emission model on sc2 by frezzing arrangement model learned from sc1
+    data_sc2, Xdesign_sc2, _ = get_mdtb_data(ses_id='ses-s2')
+    Xdesign_sc2 = pt.tensor(Xdesign_sc2)
+    em_model2 = em.MixVMF(K=K, N=40, P=P, X=Xdesign_sc2, uniform_kappa=True)
+    em_model2.initialize(data_sc2)
+    em_model2.Mstep(prior)  # give a good starting value by U_hat learned from sc1
+    #M2 = fm.FullModel(M.arrange, em_model2)
+    # M2, _, _, U_hat_sc2 = M2.fit_em(Y=data_sc2, iter=max_iter, tol=0.00001, fit_arrangement=False)
+
+    group_baseline = ev.coserr(pt.tensor(data_sc2),
+                               pt.matmul(Xdesign_sc2, em_model2.V), prior,
+                               adjusted=True, soft_assign=True)
+    lower_bound = ev.coserr(pt.tensor(data_sc2),
+                            pt.matmul(Xdesign_sc2, em_model2.V),
+                            pt.softmax(em_model2.Estep(Y=data_sc2, pure_compute=True), dim=1),
+                            adjusted=True, soft_assign=True)
+
+    ############################# Cross-validation starts here #############################
+    # Make inference on the selected run's data - run_infer
+    Data_cv, Xdesign_cv, D_cv = get_mdtb_data(ses_id='ses-s1', type='CondRun')
+    Xdesign_cv = pt.tensor(Xdesign_cv)
+    indices, cos_em, cos_complete, uhat_em_all, uhat_complete_all = [],[],[],[],[]
+    for i in runs:
+        indices.append(np.asarray(np.where(D_cv.run==i)).reshape(-1))
+        acc_run_idx = np.concatenate(indices).ravel()
+        M.emissions[0].X = Xdesign_cv[acc_run_idx,:]
+
+        # Infer on current accumulated runs data
+        U_hat_em = M.emissions[0].Estep(Y=Data_cv[:,acc_run_idx,:], pure_compute=True)
+        U_hat_complete, _ = M.arrange.Estep(U_hat_em)
+        # U_hat_complete, _ = M.Estep(Y=Data_cv[:,acc_run_idx,:])
+        uhat_em_all.append(U_hat_em)
+        uhat_complete_all.append(U_hat_complete)
+
+        if i == 1:
+            UEM = pt.argmax(U_hat_em, dim=1)+1
+            UALL = pt.argmax(U_hat_complete, dim=1) + 1
+            _plot_maps(UEM, sub=1, color=True, render_type='matplotlib',
+                       save=f'emiloglik_only_{1}_run1.pdf')
+            _plot_maps(UALL, sub=1, color=True, render_type='matplotlib',
+                       save=f'all_{1}_run1.pdf')
+
+        if i == 16:
+            UEM = pt.argmax(U_hat_em, dim=1)+1
+            UALL = pt.argmax(U_hat_complete, dim=1) + 1
+            _plot_maps(UEM, sub=1, color=True, render_type='matplotlib',
+                       save=f'emiloglik_only_{1}_run16.pdf')
+            _plot_maps(UALL, sub=1, color=True, render_type='matplotlib',
+                       save=f'all_{1}_run16.pdf')
+
+        # Calculate cosine error/u abs error between another test data
+        # and U_hat inferred from testing
+        coserr_Uem = ev.coserr(pt.tensor(data_sc2),
+                               pt.matmul(Xdesign_sc2, em_model2.V),
+                               pt.softmax(U_hat_em, dim=1),
+                               adjusted=True, soft_assign=True)
+        coserr_Uall = ev.coserr(pt.tensor(data_sc2),
+                                pt.matmul(Xdesign_sc2, em_model2.V), U_hat_complete,
+                                adjusted=True, soft_assign=True)
+
+        cos_em.append(coserr_Uem)
+        cos_complete.append(coserr_Uall)
+        # uerr_Uem = ev.rmse_YUhat(U_hat_em, pt.tensor(Data[:, 58:90, :]),
+        #                          M.emission.V[29:61, :])
+        # uerr_Uall = ev.rmse_YUhat(U_hat_complete, pt.tensor(Data[:, 58:90, :]),
+        #                          M.emission.V[29:61, :])
+
+    return group_baseline, lower_bound, cos_em, cos_complete, uhat_em_all, uhat_complete_all
+
+def learn_mdtb_hcp(K=10, max_iter=100, sub_train=None, sub_test=None, do_plot=True):
+    """Run this function to replicate the figure 5 in the nips paper
+    Args:
+        K: the number of parcels
+        max_iter: maximum iterations for EM learning
+        runs: the number of runs data to use (accumulative)
+        sub: None
+        do_plot: plot if True
+    Returns:
+        group_baseline: The cosine error for the group baseline
+        lower_bound: The noise floor
+        cos_em: The cosine error for the data only
+        cos_complete: The cosine error for data+prior
+        uhat_em_all: The exepcation obtained by data only
+        uhat_complete_all: The expecation obtained by data+prior
+    """
+    Data_1, Xdesign_1, D1 = get_mdtb_data(ses_id='ses-s1')
+    Data_2, Xdesign_2, D2 = get_mdtb_data(ses_id='ses-s2')
+    Xdesign = pt.tensor(block_diag(Xdesign_1, Xdesign_2))
+    Data = np.concatenate((Data_1, Data_2), axis=1)
+    D1['sess'] = 1  # Add a column of sess = 1
+    D2['sess'] = 2  # Add a column of sess = 2
+
+    Data_hcp = get_hcp_data(res=162, index=sub_train, runs=[0,1,2,3]) #TODO
+    # Make arrangement model and initialize the prior from the MDTB map
+    P = Data.shape[2]
+    prior_w = 7.0  # Weight of prior
+    mdtb_parcel, mdtb_colors = get_mdtb_parcel(do_plot=False)
+    logpi = ar.expand_mn(mdtb_parcel.reshape(1, P) - 1, K)
+    logpi = logpi.squeeze() * prior_w
+    # Set parcel 0 to unassigned
+    logpi[:, mdtb_parcel == 0] = 0
+
+    # Train on sc 1 data
+    ar_model = ar.ArrangeIndependent(K=K, P=P, spatial_specific=True,
+                                         remove_redundancy=False)
+    em_model = em.MixVMF(K=K, N=40, P=P, X=Xdesign, uniform_kappa=True)
+    em_model.initialize(Data)
+    em_model_2 = em.MixVMF(K=K, N=Data_hcp.shape[1], P=P, uniform_kappa=True)
+    em_model_2.initialize(Data_hcp)
+
+    # Initilize parameters from group prior and train the m odel
+    mdtb_prior = logpi.softmax(dim=0)
+    M = fm.FullMultiModel(ar_model, [em_model, em_model_2])
+    M.pre_train([Data, Data_hcp], iter=10, prior=mdtb_prior)
+    M, ll, theta, U_hat = M.fit_em(Y=[Data, Data_hcp], iter=max_iter, tol=0.00001,
+                                      fit_arrangement=True)
+    plt.plot(ll, color='b')
+    plt.show()
+
+    ### fig a: Plot group prior
+    _plot_maps(pt.argmax(M.arrange.logpi, dim=0) + 1, color=True, render_type='matplotlib',
+               save='group_prior.pdf')
+    prior = pt.softmax(M.arrange.logpi, dim=0)
+
+    # train emission model on sc2 by frezzing arrangement model learned from sc1
+    data_sc2 = get_hcp_data(res=162, index=sub_test, runs=[0, 1, 2, 3]) # TODO
+    em_model2 = em.MixVMF(K=K, N=data_sc2.shape[1], P=P, uniform_kappa=True)
+    em_model2.initialize(data_sc2)
+    em_model2.Mstep(prior.unsqueeze(0).repeat(data_sc2.shape[0], 1, 1))  # give a good starting value by U_hat learned from sc1
+    #M2 = fm.FullModel(M.arrange, em_model2)
+    # M2, _, _, U_hat_sc2 = M2.fit_em(Y=data_sc2, iter=max_iter, tol=0.00001, fit_arrangement=False)
+
+    group_baseline = ev.coserr(pt.tensor(data_sc2),
+                               em_model2.V, prior,
+                               adjusted=True, soft_assign=True)
+    lower_bound = ev.coserr(pt.tensor(data_sc2),
+                            em_model2.V,
+                            pt.softmax(em_model2.Estep(Y=data_sc2, pure_compute=True), dim=1),
+                            adjusted=True, soft_assign=True)
+
+    ############################# Cross-validation starts here #############################
+    # Make inference on the selected run's data - run_infer
+    # cos_em, cos_complete, uhat_em_all, uhat_complete_all = [],[],[],[]
+    #
+    # # Infer on current accumulated runs data
+    # U_hat_em = M.emissions[1].Estep(Y=Data_hcp, pure_compute=True)
+    # U_hat_complete, _ = M.arrange.Estep(U_hat_em)
+    # # U_hat_complete, _ = M.Estep(Y=Data_cv[:,acc_run_idx,:])
+    # uhat_em_all.append(U_hat_em)
+    # uhat_complete_all.append(U_hat_complete)
+    #
+    # UEM = pt.argmax(U_hat_em, dim=1)+1
+    # UALL = pt.argmax(U_hat_complete, dim=1) + 1
+    # _plot_maps(UEM, sub=0, color=True, render_type='matplotlib',
+    #            save=f'emiloglik_only_hcp.pdf')
+    # _plot_maps(UALL, sub=0, color=True, render_type='matplotlib',
+    #            save=f'all_hcp.pdf')
+    #
+    # # Calculate cosine error/u abs error between another test data
+    # # and U_hat inferred from testing
+    # coserr_Uem = ev.coserr(pt.tensor(data_sc2), em_model2.V, pt.softmax(U_hat_em, dim=1),
+    #                        adjusted=True, soft_assign=True)
+    # coserr_Uall = ev.coserr(pt.tensor(data_sc2), em_model2.V, U_hat_complete,
+    #                         adjusted=True, soft_assign=True)
+    #
+    # cos_em.append(coserr_Uem)
+    # cos_complete.append(coserr_Uall)
+
+    return group_baseline, lower_bound
 
 def figure_indiv_group(): 
-    D = pd.read_csv('scripts/indiv_group_err.csv')
+    D = pd.read_csv('indiv_group_err.csv')
     nf = D['noise_floor'].mean()
     gm = D['group map'].mean()
     T=pd.DataFrame()
@@ -401,17 +620,22 @@ def figure_indiv_group():
     pass 
 
 if __name__ == "__main__":
-    # Data_HCP = get_hcp_data(ses_id=['ses-01', 'ses-02'], range=np.arange(77, 100), save=True)
-    # data = get_hcp_data_from_csv(tessel=162, ses_id=['ses-01'], range=[0])
-    # data = data[0, :, :].cpu().detach().numpy()
-    # _plot_maps(data, sub=1, stats='nanmean', overlay='func', color=None, save=None)
-
+    # k_fold = 5
+    # a = np.arange(0,100)
+    # for i in range(k_fold):
+    #     b = np.arange(i*20, (i+1)*20)
+    #     A = [j for j in a if j not in b]
+    #     gbase, lb = learn_mdtb_hcp(K=10, sub_train=A, sub_test=b)
+    #
+    #     df1 = pt.cat((gbase.reshape(1,-1),lb.reshape(1,-1)), dim=0)
+    #     df1 = pd.DataFrame(df1).to_csv(f'coserrs_{i}.csv')
 
     # gbase, lb, cos_em, cos_complete, uhat_em_all, uhat_complete_all = learn_runs(K=10,
     #                                                                       runs=np.arange(1, 17))
     # df1 = pt.cat((gbase.reshape(1,-1),lb.reshape(1,-1), pt.stack(cos_em), pt.stack(cos_complete)), dim=0)
-    # df1 = pd.DataFrame(df1).to_csv('coserrs.csv')
-    #
+    # df1 = pd.DataFrame(df1).to_csv(f'coserrs_{i}.csv')
+
+    # gbase, lb = learn_mdtb_hcp(K=10, sub_train=range(0,1), sub_test=range(20,21))
     # pt.save(pt.stack(uhat_em_all), 'uhat_em_all.pt')
     # pt.save(pt.stack(uhat_complete_all), 'uhat_complete_all.pt')
     # plt.figure()
@@ -433,6 +657,7 @@ if __name__ == "__main__":
     # plt.show()
 
     # # Save as dataframe properly
+    # data = pd.read_csv('coserrs.csv')
     # nsub_list = np.arange(24)
     # labels_col = ['sub_' + f'{x+1:02}' for x in nsub_list]
     #
@@ -440,7 +665,7 @@ if __name__ == "__main__":
     # labels_row = ['group map', 'noise_floor'] + ['dataOnly_run_' + f'{x:02}' for x in runs] + ['data+prior_run_' + f'{x:02}' for x in runs]
     # data.columns = labels_col
     # data.index = labels_row
-    # data.to_csv("cosine_err.csv")
+    # data.T.to_csv("cosine_err.csv")
     # print(data)
     figure_indiv_group()
     pass
