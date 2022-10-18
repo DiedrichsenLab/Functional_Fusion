3af3cfe1f8b54f949d18c06745027a3a43121a0e
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2022-10-18 16:49:46 -0400

    Plot best Parcel

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -20,9 +20,8 @@ if not Path(base_dir).exists():
     base_dir = '/srv/diedrichsen/data/FunctionalFusion'
 
 
-def load_batch_fit(name,atl_name,K):
-    wdir = base_dir + '/Models'
-    fname = f'/{name}_space-{atl_name}_K-{K}'
+def load_batch_fit(fname):
+    wdir = base_dir + '/Models/'
     info = pd.read_csv(wdir + fname + '.tsv',sep='\t')
     with open(wdir + fname + '.pickle','rb') as file:
         models = pickle.load(file)
@@ -37,7 +36,7 @@ def load_batch_fit(name,atl_name,K):
         # Now switch the emission models accordingly:
         for j,em in enumerate(M.emissions):
             if i==0:
-                V.append(pt.zeros((n_iter,em.M,K)))
+                V.append(pt.zeros((n_iter,em.M,info.K[i])))
             V[j][i,:,:]=em.V
     return info,models,Prop,V
 
@@ -54,18 +53,41 @@ def plot_parcel_flat(data,suit_atlas,grid,map_space='SUIT'):
         plt.subplot(grid[0],grid[1],i+1)
         suit.flatmap.plot(surf_data[:,i], render='matplotlib',cmap=MDTBcolors, new_figure=False,overlay_type='label')
 
-if __name__ == "__main__":
-
-
+def plot_parcel_flat_best(model_names,grid):
     mask = base_dir + '/Atlases/tpl-MNI152NLIn2000cSymC/tpl-MNISymC_res-3_gmcmask.nii'
     atlas = am.AtlasVolumetric('MNISymC3',mask_img=mask)
+    sym_atlas = am.AtlasVolumeSymmetric('MNISymC3',mask_img=mask)
+
+    parcel=np.empty((len(model_names),atlas.P))
+
+    for i,mn in enumerate(model_names):
+        info,models,Prop,V = load_batch_fit(mn)
+        j=np.argmax(info.loglik)
+        par = pt.argmax(Prop[j,:,:],dim=0)+1 # Get winner take all 
+        # If symmetric - project back to full map: 
+        if mn[0:3]=='sym':
+            par=par[sym_atlas.indx_reduced] # Put back into full space
+        parcel[i,:]=par
+    plot_parcel_flat(parcel,atlas,grid=grid,map_space='MNISymC') 
+        
+
+
+if __name__ == "__main__":
+    model_name = ['asym_Md_space-MNISymC3_K-10',
+                  'asym_Po_space-MNISymC3_K-10',
+                  'asym_Ni_space-MNISymC3_K-10',
+                  'asym_MdPoNi_space-MNISymC3_K-10']
+    
+    plot_parcel_flat_best(model_name,[2,2])
+    # mask = base_dir + '/Atlases/tpl-MNI152NLIn2000cSymC/tpl-MNISymC_res-3_gmcmask.nii'
+    # atlas = am.AtlasVolumetric('MNISymC3',mask_img=mask)
 
     #sess = [['ses-s1'],['ses-01'],['ses-01','ses-02']]
     #design_ind= ['cond_num_uni','task_id',',..']
-    info,models,Prop,V = load_batch_fit('asym_Md','MNISymC3',10)
-    parcel = pt.argmax(Prop,dim=1)+1 # Get winner take all 
+    # info,models,Prop,V = load_batch_fit('asym_Md','MNISymC3',10)
+    # parcel = pt.argmax(Prop,dim=1)+1 # Get winner take all 
     # parcel=parcel[:,sym_atlas.indx_reduced] # Put back into full space
-    plot_parcel_flat(parcel[0:3,:],atlas,grid=[2,3],map_space='MNISymC') 
+    # plot_parcel_flat(parcel[0:3,:],atlas,grid=[2,3],map_space='MNISymC') 
     # pass
     # pass
     # Prop, V = fit_niter(data,design,K,n_iter)
