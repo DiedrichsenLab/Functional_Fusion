fc479988511c4872843fc4afe7c48cf92e5b5b31
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2022-11-23 17:25:08 -0500

    Fixed pontine import

--- scripts/import_data_pontine.py
+++ scripts/import_data_pontine.py
@@ -14,27 +14,29 @@ import scipy.io as sio
 from import_data import *
 
 if __name__ == '__main__':
-    src_base_dir = '/Volumes/diedrichsen_data$/data/Cerebellum/Pontine/'
+    src_base_dir = '/Volumes/diedrichsen_data$/data/Cerebellum/Pontine7T/'
     dest_base_dir = '/Volumes/diedrichsen_data$/data/FunctionalFusion/Pontine/'
-    for participant_id in ['01', '03', '04', '07', '95', '96', '97', '98']:
+    for participant_id in [ '98']:
+        # '01', '03', '04', '07', '95', '96', '97',
 
-        # # --- Importing SUIT ---
-        source_dir = '{}/suit/anatomicals/S{}'.format(src_base_dir, participant_id)
-        dest_dir = '{}/derivatives/sub-{}/suit'.format(dest_base_dir, participant_id)
-        anat_name = 'anatomical'
-        import_suit(source_dir,dest_dir,anat_name,'sub-' + participant_id)
-        pass
+        # # # --- Importing SUIT ---
+        # source_dir = '{}/suit/anatomicals/S{}'.format(src_base_dir, participant_id)
+        # dest_dir = '{}/derivatives/sub-{}/suit'.format(dest_base_dir, participant_id)
+        # anat_name = 'anatomical'
+        # import_suit(source_dir,dest_dir,anat_name,'sub-' + participant_id)
+
         # # --- Importing ANAT ---
         # source_dir = '{}/anatomicals/S{}'.format(src_base_dir, participant_id)
         # dest_dir = '{}/derivatives/sub-{}/anat'.format(dest_base_dir, participant_id)
         # anat_name = 'anatomical'
         # import_anat(source_dir,dest_dir,anat_name,participant_id)
 
-        # # --- Importing Freesurfer ---
-        # source_dir = '{}/surfaceFreesurfer/S{}/surf'.format(src_base_dir, participant_id)
-        # dest_dir = '{}/derivatives/sub-{}/anat'.format(dest_base_dir, participant_id)
-        # new_id = 'sub-{}'.format(participant_id)
-        # import_freesurfer(source_dir,dest_dir,new_id)
+        # --- Importing Freesurfer ---
+        source_dir = '{}/surfaceWB/data/S{}/'.format(src_base_dir, participant_id)
+        dest_dir = '{}/derivatives/sub-{}/anat'.format(dest_base_dir, participant_id)
+        old_id = 'S{}'.format(participant_id)
+        new_id = 'sub-{}'.format(participant_id)
+        import_freesurfer(source_dir,dest_dir,old_id,new_id)
 
         # --- Importing Estimates ---
         #source_dir = '{}/GLM_firstlevel_2/S{}/'.format(src_base_dir, #participant_id)
