09663f69926f2b21e6b29ea17ddc8bd8ef8b5673
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2022-12-16 10:54:47 -0500

    Update import_data_somatotopic.py

--- scripts/import_data_somatotopic.py
+++ scripts/import_data_somatotopic.py
@@ -16,6 +16,8 @@ from Functional_Fusion.dataset import DataSetSomatotopic
 import Functional_Fusion.atlas_map as am
 import shutil
 import nibabel as nb
+from copy import deepcopy
+import subprocess
 
 base_dir = '/Volumes/diedrichsen_data$/data'
 if not Path(base_dir).exists():
@@ -26,16 +28,36 @@ src_base_dir = Path(base_dir + '/Cerebellum//Somatotopic')
 dest_base_dir = Path(base_dir + '/FunctionalFusion/Somatotopic')
 
 
+def create_reginfo():
+    dataset = DataSetSomatotopic(str(dest_base_dir))
+    info = pd.read_csv(dest_base_dir / 'ses-all_reginfo.tsv', sep='\t')
+    T = dataset.get_participants()
+    for _, id in T.iterrows():
+        print(f'Importing {id.participant_id}')
+        # Ammend the reginfo.tsv file from the general file
+        reginfo = deepcopy(info)
+        reginfo.insert(loc=0, column='sn', value=[
+            id.participant_id] * info.shape[0])
+        for session in np.arange(4):
+            # for session in [3]:
+            # Save reginfo.tsv file
+            reginfo.to_csv(dest_base_dir /
+                           f'derivatives/{id.participant_id}/estimates/ses-{session+1:02d}/{id.participant_id}_ses-{session+1:02d}_reginfo.tsv', sep='\t', index=False)
+
 def import_data():
     dataset = DataSetSomatotopic(str(dest_base_dir))
     info = pd.read_csv(dest_base_dir / 'ses-all_reginfo.tsv', sep='\t')
     T = dataset.get_participants()
     for _, id in T.iterrows():
         print(f'Importing {id.participant_id}')
-        # for session in np.arange(4):
-        for session in [3]:
+        for session in np.arange(4):
+            # for session in [3]:
+            # Save reginfo.tsv file
+            reginfo = pd.read_csv(dest_base_dir /
+                        f'derivatives/{id.participant_id}/estimates/ses-{session+1:02d}/{id.participant_id}_ses-{session+1:02d}_reginfo.tsv', sep='\t')
             resms = []
-            for run in np.arange(info.run.max()):
+            for _, run, _, pe, _, reg_id in reginfo.iterrows():
+            # for run in np.arange(info.run.max()):
                 # for pe in info.pe_id.unique():
 
                 #     src = src_base_dir / \
@@ -44,7 +66,7 @@ def import_data():
 
                 #     reg=info[info.pe_id == pe].reg_id.iloc[0]
                 #     dest = dest_base_dir / \
-                #         f'derivatives/{id.participant_id}/estimates/ses-{session+1:02d}/{id.participant_id}_ses-{session+1:02d}_run-{run+1:02d}_reg-{reg:02d}_beta.nii'
+                #         f'derivatives/{id.participant_id}/estimates/ses-{session+1:02d}/{id.participant_id}_ses-{session+1:02d}_run-{run+1:02d}_reg-{reg:02d}_beta.nii.gz'
                     
                 #     # Make folder
                 #     dest.parent.mkdir(parents=True, exist_ok=True)
@@ -57,25 +79,22 @@ def import_data():
                 #         except:
                 #             print('skipping ' + str(src))
 
+                #     subprocess.call(
+                #         ['gunzip', '-f', dest])
 
                 
-                src = src_base_dir / \
-                    f'raw/Functional/{id.orig_id}/{id.orig_id}_sess{session+1:02d}_MOTOR{run+1}/sigmasquareds.nii.gz'
+                # src = src_base_dir / \
+                #     f'raw/Functional/{id.orig_id}/{id.orig_id}_sess{session+1:02d}_MOTOR{run+1}/sigmasquareds.nii.gz'
             #     resms_img = nb.load(src)
             #     resms.append(resms_img.get_fdata())
 
             # resms = np.mean(resms,axis=0)
             # nifti_img = nb.Nifti1Image(dataobj=resms, affine=resms_img.affine)
-            outname = dest_base_dir / \
-                f'derivatives/{id.participant_id}/estimates/ses-{session+1:02d}/{id.participant_id}_ses-{session+1:02d}_resms.nii'
+            # outname = dest_base_dir / \
+            #     f'derivatives/{id.participant_id}/estimates/ses-{session+1:02d}/{id.participant_id}_ses-{session+1:02d}_resms.nii'
             # nb.save(nifti_img, outname)
             
-            # Import the reginfo.tsv file from the general file
-            info.insert(loc=0, column='sn', value=[id.participant_id]*info.shape[0])
-            info.to_csv(dest_base_dir /
-                        f'derivatives/{id.participant_id}/estimates/ses-{session+1:02d}/{id.participant_id}_ses-{session+1:02d}_reginfo.tsv', sep='\t', index=False)
 
-            pass
 
 
         
