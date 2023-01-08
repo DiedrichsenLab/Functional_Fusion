7844051a82a4c647da86c9b6ddc8fd9678c899e4
lshahsha <lshahsha@uwo.ca>
2023-01-08 13:29:49 -0500

    added a function to aggregate data for both cereb and cortex

--- scripts/test_parcel.py
+++ scripts/test_parcel.py
@@ -9,7 +9,6 @@ from dataset import DataSetMDTB
 from dataset import DataSetHcpResting
 import nibabel as nb
 from matrix import indicator
-import sys
 import os
 import sys
 from nibabel import cifti2
@@ -58,12 +57,12 @@ def test_mdtb_fs32k():
     return fs_atlas
 
 # extracting average within a parcel:
-# 1. load the extracted data
-# 2. use the label image to create a parcel axis for the space 
-# 3. calculate mean for each parcel
+# 1. load the extracted data DONE
+# 2. use the label image to create a parcel axis for the space DONE
+# 3. calculate mean for each parcel 
 # 4. save! 
 
-def extract_parcel_data():
+def extract_parcel_data_suit():
     """
     Example code to extract mean across parcel for mdtb suit
     """
@@ -98,11 +97,55 @@ def extract_parcel_data():
     header = nb.Cifti2Header.from_axes((row_axis, pa))
     cifti_img = nb.Cifti2Image(dataobj=data_parcel, header=header)
     nb.save(cifti_img, 'test_mdtb_10.pscalar.nii')
-    print("DEVELOPING")
+    A = nb.load('test_mdtb_10.pscalar.nii')
+    return data_parcel
+
+def extract_parcel_data():
+    """
+    Example code to extract mean across parcel for tessellation
+    """
+    # perform all the steps in test_mdtb_suit() to create a parcel axis for the parcellation
+    atlas_parcel = test_mdtb_fs32k()
+
+    # load in the data for sub-02
+    data_dir = project_dir + '/derivatives' + '/sub-02' +  '/data' + '/sub-02_space-fs32k_ses-s1_CondAll.dscalar.nii'
+    data_img = nb.load(data_dir)
+    # get data for the structure
+    data = data_img.get_fdata()
+
+    # load the tsv file
+    info_file = project_dir + '/derivatives' + '/sub-02' +  '/data' + '/sub-02_ses-s1_info-CondAll.tsv'
+    info_tsv = pd.read_csv(info_file, sep = '\t')
+
+    if len(atlas_parcel.label_vector) == 1: # for cerebellum (not divided by hemi)
+        vector = atlas_parcel.label_vector[0]
+    else: # for cortex, divided into left and right
+        vector = np.concatenate(atlas_parcel.label_vector, axis = 0)
+
+    # create a matrix for aggregating data (cannot use dataset.agg_data now! Need to make changes)
+    C = indicator(vector,positive=True)
+
+    # get the mean across parcel
+    data = np.nan_to_num(data)
+    data_parcel = (data @ C)/np.sum(C, axis = 0)
+
+    # create a dscale cifti with parcelAxis labels and data_parcel
+    row_axis = info_tsv.cond_name
+    row_axis = nb.cifti2.ScalarAxis(row_axis)
+        
+
+    bm = atlas_parcel.get_brain_model_axis()
+    pa = atlas_parcel.parcel_axis
+    # HEAD = cifti2.Cifti2Header.from_axes((row_axis,bm,pa))
+    header = nb.Cifti2Header.from_axes((row_axis, pa))
+    cifti_img = nb.Cifti2Image(dataobj=data_parcel, header=header)
+    nb.save(cifti_img, 'test_mdtb_10.pscalar.nii')
+    A = nb.load('test_mdtb_10.pscalar.nii')
     return data_parcel
 
 if __name__ == "__main__":
     # a = test_mdtb_suit()
     # b = test_mdtb_fs32k()
+    # extract_parcel_data_suit()
     extract_parcel_data()
     print("hovering")
