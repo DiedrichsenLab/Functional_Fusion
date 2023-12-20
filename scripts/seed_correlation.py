245c21b13de42c234fe80b9d81cc0167fc6e35ae
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2023-12-20 12:44:33 -0500

    Updated rest

--- scripts/seed_correlation.py
+++ scripts/seed_correlation.py
@@ -3,17 +3,16 @@ from pathlib import Path
 import os.path as op
 import preprocessing.paths as paths
 import Functional_Fusion.util as util
+import numpy as np
 
-# create seed-based correlation map
-# extract voxel timeseries in native space
-
+# set paths
+base_dir = paths.set_base_dir()
+rest_dir = f'{base_dir}/../Cerebellum/super_cerebellum/resting_state/'
 
 def extract_voxel_timeseries(data, coord):
     x, y, z = coord
     voxel_timeseries = data[x, y, z, :]
     return voxel_timeseries
-
-# correlate with all other voxels in native space
 
 
 def correlate_with_all_voxels(X, Y):
@@ -21,16 +20,7 @@ def correlate_with_all_voxels(X, Y):
     Y = util.zstandarize_ts(Y)
     return Y.T @ X / X.shape[0]
 
-# z-transform correlation map
-
-
-def z_transform_correlation_map(correlation_map):
-    # TODO: Implement the z-transform of the correlation map
-    pass
-    return correlation_map
-
-
-coordinates = {
+cerebellum_coordinates = {
     's02': (36, 23, 20),
     's03': (38, 21, 17),
     's06': (37, 21, 18),
@@ -61,49 +51,75 @@ coordinates = {
 
 }
 
-subjects_runs = {
-    's06': [1],
-    's07': [1],
-    's08': [2],
-    's10': [2]}
 
-if __name__ == "__main__":
-    # set paths
-    base_dir = paths.set_base_dir()
-    rest_dir = f'{base_dir}/../Cerebellum/super_cerebellum/resting_state/'
+def get_corrmap(subject, run, clean=False, roi='cerebellum', save=False):
+    """Get correlation map for a given subject and run.
+        
+        Args:
+            subject (str): subject ID
+            run (int): run number
+            clean (bool): whether to use clean data
+            roi (str): ROI to use as seed
+            save (bool): whether to save the correlation map
+            
+        Returns:
+            correlation_map (np.ndarray): correlation map
+    """
+    # Load data
+    data_path = f'{rest_dir}/imaging_data/{subject}/run{run:02d}.feat/filtered_func_data.nii.gz'
+    if clean:
+        data_path = f'{rest_dir}/imaging_data/{subject}/run{run:02d}.feat/filtered_func_data_clean.nii.gz'
+    data = nib.load(data_path).get_fdata()
+
+    # Extract voxel timeseries
+    if roi == 'cerebellum':
+        seed_coord = cerebellum_coordinates[subject]
+    else:
+        seed_coord = cerebellum_coordinates[subject]
+        seed_coord = (seed_coord[0], seed_coord[1], seed_coord[2] + 5)
+    voxel_timeseries = extract_voxel_timeseries(data, seed_coord)
+    # voxel_timeseries = voxel_timeseries.reshape((voxel_timeseries.shape[0], 1)).T
+
+    # Load mask that excludes non-brain and non-skull background voxels
+    mask_path = f'{rest_dir}/imaging_data/{subject}/run{run:02d}.feat/mask.nii.gz'
+    mask = nib.load(mask_path).get_fdata()
+    data = data[mask > 0]
+
+    # Correlate with all other voxels
+    correlation_map = correlate_with_all_voxels(
+        data.T, voxel_timeseries)
+    correlation_map[np.isnan(correlation_map)] = 0
+
+    # Z-transform correlation map
+    # z_transformed_map = z_transform_correlation_map(correlation_map)
+    
+    # Save correlation map
+    save_path = f'{rest_dir}/fix_ica/corrmap-{roi[0]}_{subject}-run{run:02d}_prefix.nii.gz'
+    if clean:
+        save_path = f'{rest_dir}/fix_ica/corrmap-{roi[0]}_{subject}-run{run:02d}_postfix.nii.gz'
+    correlation_img = mask.copy()
+    correlation_img[mask > 0] = correlation_map
+    correlation_image = nib.Nifti1Image(
+        correlation_img, affine=nib.load(mask_path).affine)
+    # Save nibabel image
+    nib.save(correlation_image, save_path)
+    return correlation_map
+
+def get_all_corrmaps(save=False, roi='cerebellum'):
+    """Get correlation maps for all subjects and runs.
+        
+        Args:
+            save (bool): whether to save the correlation maps
+            roi (str): ROI to use as seed
+    """
+    correlations_maps = []
     for s, subject in enumerate(rest_dir.glob('s*')):
         for run in [1, 2]:
             for clean in [True, False]:
-                # Load data
-                data_path = f'{rest_dir}/imaging_data/{subject}/run{run:02d}.feat/filtered_func_data.nii.gz'
-                if clean:
-                    data_path = f'{rest_dir}/imaging_data/{subject}/run{run:02d}.feat/filtered_func_data_clean.nii.gz'
-                data = nib.load(data_path).get_fdata()
+                correlation_map = get_corrmap(subject, run, clean=clean, roi=roi, save=save)
+                correlations_maps.append(correlation_map)
 
-                # Extract voxel timeseries
-                seed_coord = coordinates[subject]
-                voxel_timeseries = extract_voxel_timeseries(data, seed_coord)
-                # voxel_timeseries = voxel_timeseries.reshape((voxel_timeseries.shape[0], 1)).T
-
-                # Load mask that excludes non-brain and non-skull background voxels
-                mask_path = f'{rest_dir}/imaging_data/{subject}/run{run:02d}.feat/mask.nii.gz'
-                mask = nib.load(mask_path).get_fdata()
-                data = data[mask > 0]
-
-                # Correlate with all other voxels
-                correlation_map = correlate_with_all_voxels(
-                    data.T, voxel_timeseries)
-
-                # Z-transform correlation map
-                # z_transformed_map = z_transform_correlation_map(correlation_map)
-
-                # Save correlation map
-                save_path = f'{rest_dir}/fix_ica/correlation_map_{subject}-run{run:02d}.nii.gz'
-                if clean:
-                    save_path = f'{rest_dir}/fix_ica/correlation_map_{subject}-run{run:02d}_clean.nii.gz'
-                correlation_img = mask.copy()
-                correlation_img[mask > 0] = correlation_map
-                correlation_image = nib.Nifti1Image(
-                    correlation_img, affine=nib.load(mask_path).affine)
-                # Save nibabel image
-                nib.save(correlation_image, save_path)
+if __name__ == "__main__":
+    get_all_corrmaps(save=True, roi='cerebellum')
+    get_all_corrmaps(save=True, roi='occipital')
+    
