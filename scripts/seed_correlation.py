227d261f7b702122f4e67de1bf7156f897e8a709
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2023-12-20 12:25:20 -0500

    Updated rest

--- scripts/seed_correlation.py
+++ scripts/seed_correlation.py
@@ -6,22 +6,29 @@ import Functional_Fusion.util as util
 
 # create seed-based correlation map
 # extract voxel timeseries in native space
+
+
 def extract_voxel_timeseries(data, coord):
     x, y, z = coord
     voxel_timeseries = data[x, y, z, :]
     return voxel_timeseries
 
 # correlate with all other voxels in native space
+
+
 def correlate_with_all_voxels(X, Y):
     X = util.zstandarize_ts(X)
     Y = util.zstandarize_ts(Y)
     return Y.T @ X / X.shape[0]
 
 # z-transform correlation map
+
+
 def z_transform_correlation_map(correlation_map):
     # TODO: Implement the z-transform of the correlation map
     pass
     return correlation_map
+
 
 coordinates = {
     's02': (36, 23, 20),
@@ -59,14 +66,14 @@ subjects_runs = {
     's07': [1],
     's08': [2],
     's10': [2]}
+
+
 if __name__ == "__main__":
     # set paths
     base_dir = paths.set_base_dir()
     rest_dir = f'{base_dir}/../Cerebellum/super_cerebellum/resting_state/'
-    # for s, subject in enumerate(rest_dir.glob('s*')):
-    for s, subject in enumerate(subjects_runs.keys()):
-        # for run in [1, 2]:
-        for run in subjects_runs[subject]:
+    for s, subject in enumerate(rest_dir.glob('s*')):
+        for run in [1, 2]:
             for clean in [True, False]:
                 # Load data
                 data_path = f'{rest_dir}/imaging_data/{subject}/run{run:02d}.feat/filtered_func_data.nii.gz'
@@ -84,9 +91,9 @@ if __name__ == "__main__":
                 mask = nib.load(mask_path).get_fdata()
                 data = data[mask > 0]
 
-
                 # Correlate with all other voxels
-                correlation_map = correlate_with_all_voxels(data.T, voxel_timeseries)
+                correlation_map = correlate_with_all_voxels(
+                    data.T, voxel_timeseries)
 
                 # Z-transform correlation map
                 # z_transformed_map = z_transform_correlation_map(correlation_map)
@@ -97,6 +104,7 @@ if __name__ == "__main__":
                     save_path = f'{rest_dir}/fix_ica/correlation_map_{subject}-run{run:02d}_clean.nii.gz'
                 correlation_img = mask.copy()
                 correlation_img[mask > 0] = correlation_map
-                correlation_image = nib.Nifti1Image(correlation_img, affine=nib.load(mask_path).affine)
+                correlation_image = nib.Nifti1Image(
+                    correlation_img, affine=nib.load(mask_path).affine)
                 # Save nibabel image
                 nib.save(correlation_image, save_path)
