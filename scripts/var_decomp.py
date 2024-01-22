2da880852e358720842ff63e73f8fc6b8cf554ee
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2024-01-22 18:31:53 -0500

    Update var_decomp.py

--- scripts/var_decomp.py
+++ scripts/var_decomp.py
@@ -55,8 +55,8 @@ def reshape_data(data, info, cond_column='cond_num_uni', part_column='run', mean
     # Extract each run and concatenate in third dimension
     data_reshaped = np.zeros((data.shape[0], info[part_column].max(), info[cond_column].max(), data.shape[-1]))
     for i in range(1, info.run.max()+1):
-        run_data = data[:, info.run == i, :]
-        data_reshaped[:, i-1, :, :] = run_data
+        part_data = data[:, info.run == i, :]
+        data_reshaped[:, i-1, :, :] = part_data
 
     # Set nans to number and print number of nans
     print(f'Setting {np.sum(np.isnan(data_reshaped))} nan values to zero.')
@@ -82,6 +82,8 @@ if __name__ == "__main__":
 
     # -- Variance decomposition --
     mean_centering = True
+    type='CondRun'
+    part_column='half'
     # --- Resting-state ---
     data_rest, info_rest = mdtb_dataset.get_data(ses_id='ses-rest', type='Net69Run', space='MNISymC2', subj=subject_subset)
     data_reshaped_rest = reshape_data(data_rest, info_rest, cond_column='net_id', mean_centering=mean_centering)
@@ -90,37 +92,38 @@ if __name__ == "__main__":
 
     # --- Task ---
     data_task, info_task = mdtb_dataset.get_data(ses_id='ses-s1', type='CondRun', space='MNISymC2', subj=subject_subset)
-    data_reshaped_task = reshape_data(data_task, info_task, cond_column=mdtb_dataset.cond_ind, mean_centering=mean_centering)
+    data_reshaped_task = reshape_data(data_task, info_task, cond_column=mdtb_dataset.cond_ind, part_column=part_column, mean_centering=mean_centering)
     vars_task = ds.decompose_pattern_into_group_indiv_noise(data_reshaped_task, criterion='global')
     print(f'Task variance\nGroup: {vars_task[0][0]:.2f}\nSubject: {vars_task[0][1]:.2f}\nError: {vars_task[0][2]:.2f}')
     
     # --- Task Neocortex ---
     data_task_neocortex, info_task_neocortex = mdtb_dataset.get_data(ses_id='ses-s1', type='CondRun', space='fs32k')
-    data_reshaped_task_neocortex = reshape_data(data_task_neocortex, info_task, cond_column=mdtb_dataset.cond_ind, mean_centering=mean_centering)
-    data_reshaped_task_neocortex = reshape_data(data_task_neocortex, info_task, cond_column=mdtb_dataset.cond_ind, mean_centering=False)
+    data_reshaped_task_neocortex = reshape_data(data_task_neocortex, info_task, cond_column=mdtb_dataset.cond_ind, part_column=part_column, mean_centering=mean_centering)
+    data_reshaped_task_neocortex = reshape_data(data_task_neocortex, info_task, cond_column=mdtb_dataset.cond_ind, part_column=part_column, mean_centering=False)
     
-    # Create dataset all runs are data from the first run for each subject (to maximize subject variance)
-    data_max_subject_variance = np.zeros_like(data_reshaped_task)
-    for i in range(data_reshaped_task.shape[1]):
-        data_max_subject_variance[:, i, :, :] = data_reshaped_task[:, 0, :, :]
-    vars_subject = ds.decompose_pattern_into_group_indiv_noise(data_max_subject_variance, criterion='global')
-    print(f'Same runs variance\nGroup: {vars_subject[0][0]:.2f}\nSubject: {vars_subject[0][1]:.2f}\nError: {vars_subject[0][2]:.2f}')
-
-    # Create dataset where all 24 subjects are data from the first subject (to maximize group variance)
-    data_max_group_variance = np.zeros_like(data_reshaped_task)
-    for i in range(data_reshaped_task.shape[0]):
-        data_max_group_variance[i, :, :, :] = data_reshaped_task[0, :, :, :]
-    vars_group = ds.decompose_pattern_into_group_indiv_noise(data_max_group_variance, criterion='global')
-    print(f'Same subjects variance\nGroup: {vars_group[0][0]:.2f}\nSubject: {vars_group[0][1]:.2f}\nError: {vars_group[0][2]:.2f}')
-
-    # Create dataset where error variance is maximized
-    data_max_error_variance = np.random.rand(*data_reshaped_task.shape)
-    vars_error = ds.decompose_pattern_into_group_indiv_noise(data_max_error_variance, criterion='global')
-    print(f'Random numbers variance\nGroup: {vars_error[0][0]:.2f}\nSubject: {vars_error[0][1]:.2f}\nError: {vars_error[0][2]:.2f}')
-
-
     vars_task_neo = ds.decompose_pattern_into_group_indiv_noise(data_reshaped_task_neocortex, criterion='global')
     print(f'Neocortical task variance\nGroup: {vars_task_neo[0][0]:.2f}\nSubject: {vars_task_neo[0][1]:.2f}\nError: {vars_task_neo[0][2]:.2f}')
 
+    # Create dataset all runs are data from the first run for each subject (to maximize subject variance)
+    # data_max_subject_variance = np.zeros_like(data_reshaped_task)
+    # for i in range(data_reshaped_task.shape[1]):
+    #     data_max_subject_variance[:, i, :, :] = data_reshaped_task[:, 0, :, :]
+    # vars_subject = ds.decompose_pattern_into_group_indiv_noise(data_max_subject_variance, criterion='global')
+    # print(f'Same runs variance\nGroup: {vars_subject[0][0]:.2f}\nSubject: {vars_subject[0][1]:.2f}\nError: {vars_subject[0][2]:.2f}')
+
+    # # Create dataset where all 24 subjects are data from the first subject (to maximize group variance)
+    # data_max_group_variance = np.zeros_like(data_reshaped_task)
+    # for i in range(data_reshaped_task.shape[0]):
+    #     data_max_group_variance[i, :, :, :] = data_reshaped_task[0, :, :, :]
+    # vars_group = ds.decompose_pattern_into_group_indiv_noise(data_max_group_variance, criterion='global')
+    # print(f'Same subjects variance\nGroup: {vars_group[0][0]:.2f}\nSubject: {vars_group[0][1]:.2f}\nError: {vars_group[0][2]:.2f}')
+
+    # # Create dataset where error variance is maximized
+    # data_max_error_variance = np.random.rand(*data_reshaped_task.shape)
+    # vars_error = ds.decompose_pattern_into_group_indiv_noise(data_max_error_variance, criterion='global')
+    # print(f'Random numbers variance\nGroup: {vars_error[0][0]:.2f}\nSubject: {vars_error[0][1]:.2f}\nError: {vars_error[0][2]:.2f}')
+
+
+    
     pass
     
