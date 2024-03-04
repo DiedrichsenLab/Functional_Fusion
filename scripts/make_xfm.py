6698bc24c1a9967d21f174353c9087c18db2dbc2
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2024-03-03 20:46:05 -0500

    Transforms

--- scripts/make_xfm.py
+++ scripts/make_xfm.py
@@ -1,9 +1,9 @@
 # Script for distorting a new SUIT surface for the MNISymC template
-import numpy as np 
+import numpy as np
 import nibabel as nb
 import SUITPy as suit
 import nitools as nt
-import Functional_Fusion.util as ff
+import Functional_Fusion.util as ut
 
 base_dir = '/Volumes/diedrichsen_data$/data/FunctionalFusion'
 atlas_dir = base_dir + '/Atlases'
@@ -11,8 +11,7 @@ atlas_dir = base_dir + '/Atlases'
 
 if __name__ == "__main__":
     # reslice_SUIT()
-    deform_suit_surfaces()
-
+    ut.templateflow_xfm_h5_to_nii(atlas_dir+'/tpl-MNI152NLin6Asym'+'/tpl-MNI152NLin6Asym_from-MNI152NLin2009cAsym_mode-image_xfm.h5')
     # T= pd.read_csv(data_dir + '/participants.tsv',delimiter='\t')
     # for s in T.participant_id:
     #     pass
