8cb039a862f2404cb6bba10753a3243004db5f59
Ladan Shahshahani <44071291+lshahsha@users.noreply.github.com>
2024-03-08 13:05:21 -0500

    fixed bug in NN dataset

--- Functional_Fusion/dataset.py
+++ Functional_Fusion/dataset.py
@@ -1308,27 +1308,27 @@ class DataSetNishi(DataSetNative):
 
         if type == 'CondHalf':
             data_info, C = agg_data(info,
-                                    ['half', 'cond_num'],
+                                    ['half', 'reg_id'],
                                     ['run', 'reg_num'])
             data_info['names'] = [
-                f'{d.cond_name.strip()}-half{d.half}' for i, d in data_info.iterrows()]
+                f'{d.task_name.strip()}-half{d.half}' for i, d in data_info.iterrows()]
 
             # Baseline substraction
             B = matrix.indicator(data_info.half, positive=True)
 
         elif type == 'CondRun':
             data_info, C = agg_data(info,
-                                    ['run', 'cond_num'],
-                                    [])
+                                    ['run', 'reg_id'],
+                                    ['reg_num'])
 
             data_info['names'] = [
-                f'{d.cond_name.strip()}-run{d.run:02d}' for i, d in data_info.iterrows()]
+                f'{d.task_name.strip()}-run{d.run:02d}' for i, d in data_info.iterrows()]
             # Baseline substraction
             B = matrix.indicator(data_info.run, positive=True)
         elif type == 'CondAll':
             data_info, C = agg_data(info,
-                                    ['cond_num'],
-                                    ['run', 'half', 'reg_num'])
+                                    ['reg_id'],
+                                    ['run', 'half'])
             # Baseline substraction
             B = np.ones((data_info.shape[0],))
 
@@ -1336,7 +1336,7 @@ class DataSetNishi(DataSetNative):
         data_n = prewhiten_data(data)
 
         # Load the designmatrix and perform optimal contrast
-        X = np.load(dir + f'/{participant_id}_{ses_id}_designmatrix.npy')
+        X = np.load(self.estimates_dir.format(participant_id) + f'/{ses_id}/{participant_id}_{ses_id}_designmatrix.npy')
         reg_in = np.arange(C.shape[1], dtype=int)
         data_new = optimal_contrast(data_n, C, X, reg_in, baseline=B)
 
@@ -1371,7 +1371,7 @@ class DataSetIBC(DataSetNative):
             self.base_dir + '/participants.tsv', delimiter='\t')
         return self.part_info[self.part_info.complete == 1]
 
-    def get_data_fnames(self, participant_id, session_id=None):
+    def get_data_fnames(self, participant_id, session_id=None, type = "CondHalf"):
         """ Gets all raw data files
 
         Args:
