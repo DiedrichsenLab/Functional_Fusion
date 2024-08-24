d95328d1672945a4e083b9f1e5fd87ba325e4066
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2024-08-24 16:16:10 -0400

    Update import_language.py

--- scripts/import_language.py
+++ scripts/import_language.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
         file_ending = '_run-{run}_fix.nii'
     else:
         src_stem = base_dir + '/Cerebellum/Language/Language_7T/imaging_data/{sub}/ses-04/{sub}_ses-04'
-        file_ending = '_run-{run}'
+        file_ending = '_run-{run}.nii'
         
     for s in participants:
         src = src_stem.format(sub=s) + file_ending
