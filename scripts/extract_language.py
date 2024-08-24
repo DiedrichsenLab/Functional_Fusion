0b8bc78f2d3a0cd2851821204224afa650f89d88
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2024-08-24 16:57:08 -0400

    Update extract_language.py

--- scripts/extract_language.py
+++ scripts/extract_language.py
@@ -69,8 +69,8 @@ if __name__ == "__main__":
             data_dir + '/participants.tsv', delimiter='\t')
     subject_subset = T.participant_id[T['ses-rest'] == 1].tolist()
     lang_dataset = DataSetLanguage(data_dir)   
-    lang_dataset.extract_all(ses_id='ses-rest', type='Tseries', atlas='MNISymC3', smooth=2.0)
-    lang_dataset.extract_all(ses_id='ses-rest', type='Tseries', atlas='fs32k', smooth=2.0)
+    lang_dataset.extract_all(ses_id='ses-rest', type='Tseries', atlas='MNISymC3', subj=subject_subset)
+    lang_dataset.extract_all(ses_id='ses-rest', type='Tseries', atlas='fs32k', subj=subject_subset)
     conn.get_connectivity_fingerprint(dname,
                                       type='Fus06Run', space='MNISymC3', ses_id='ses-rest', subj=subject_subset)
     pass
