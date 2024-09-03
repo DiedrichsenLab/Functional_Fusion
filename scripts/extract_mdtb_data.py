1bb4425188a02d76ff43d265cacee329ea8e1092
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2024-09-03 11:04:37 -0400

    udpated mdtb extract

--- scripts/extract_mdtb_data.py
+++ scripts/extract_mdtb_data.py
@@ -138,14 +138,14 @@ if __name__ == "__main__":
     # mdtb_dataset.extract_all(ses_id='ses-rest', type='Tseries', atlas='fs32k', smooth=2.0)
 
     # -- Extract task timeseries --
-    # mdtb_dataset.extract_all(ses_id='ses-s1', type='FixTseries', atlas='MNISymC3', smooth=2.0)
+    # mdtb_dataset.extract_all(ses_id='ses-s1', type='FixTseries', atlas='MNISymC3', smooth=2.0, subj=[23])
     # mdtb_dataset.extract_all(ses_id='ses-s1', type='FixTseries', atlas='fs32k', smooth=2.0, subj=[23])
     # mdtb_dataset.extract_all(ses_id='ses-s1', type='FixTseries', atlas='fs32k', smooth=2.0, subj=[16])
     # mdtb_dataset.extract_all(ses_id='ses-s2', type='FixTseries', atlas='MNISymC3', smooth=2.0)
-    mdtb_dataset.extract_all(ses_id='ses-s2', type='FixTseries', atlas='fs32k', smooth=2.0)
+    # mdtb_dataset.extract_all(ses_id='ses-s2', type='FixTseries', atlas='fs32k', smooth=2.0, subj=[23])
     
     conn.get_connectivity_fingerprint(dname,
-                                      type='Fus06FixRun', space='MNISymC3', ses_id='ses-s1')
+                                      type='Fus06FixRun', space='MNISymC3', ses_id='ses-s1', subj=['sub-31'])
     conn.get_connectivity_fingerprint(dname,
                                     type='Fus06FixRun', space='MNISymC3', ses_id='ses-s2')
     
