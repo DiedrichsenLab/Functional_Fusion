7d706496a5b2a25f31446951b38680ad935c39b9
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2024-09-29 19:07:40 -0400

    Update sandbox.sh

--- deprecated/sandbox.sh
+++ deprecated/sandbox.sh
@@ -8,3 +8,31 @@ for i in ${base_dir}/FunctionalFusion/MDTB/derivatives/sub*/data/*FixBeta*; do
     mv $i $newname
 
 done
+
+
+base_dir="/cifs/diedrichsen/data/"
+for i in ${base_dir}/FunctionalFusion/MDTB/derivatives/sub*/data/*ses-rest_Tseries*; do 
+    echo $i
+    # Rename the "FixBeta" part of the file to "FixNewCondRun"
+    newname=$(echo $i | sed 's/Tseries/FixTseries/')
+    echo $newname
+    mv $i $newname
+
+done
+
+
+base_dir="/cifs/diedrichsen/data/"
+for i in ${base_dir}/FunctionalFusion/MDTB/derivatives/sub*/data/*ses-rest_*Net69Run*; do 
+    echo $i
+    # Rename the "FixBeta" part of the file to "FixNewCondRun"
+    newname=$(echo $i | sed 's/Net69Run/Net69FixRun/')
+    echo $newname
+    mv $i $newname
+
+done
+
+
+Ico162Run
+Ico42Run
+Net67Run
+Net69Run
