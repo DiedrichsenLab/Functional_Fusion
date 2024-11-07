c01b72ca5a62caa49ac217fce8b681b99cf6d9f0
Jorn Diedrichsen <joern.diedrichsen@googlemail.com>
2024-11-07 09:46:08 -0500

    extraction

--- scripts/extract_mdtb_data.py
+++ scripts/extract_mdtb_data.py
@@ -104,10 +104,10 @@ def reshape_data(data, info, cond_column='cond_num_uni', part_column='run', mean
 
 if __name__ == "__main__":
     mdtb_dataset = DataSetMDTB(mdtb_dir)
-    mdtb_dataset.extract_all(ses_id='ses-s1', type='CondRun', atlas='MNISymDentate1', smooth=None)
-    mdtb_dataset.extract_all(ses_id='ses-s2', type='CondRun', atlas='MNISymDentate1', smooth=None)
     mdtb_dataset.extract_all(ses_id='ses-s1', type='CondRun', atlas='MNISymCereb2', smooth=2.0)
     mdtb_dataset.extract_all(ses_id='ses-s2', type='CondRun', atlas='MNISymCereb2', smooth=2.0)
+    mdtb_dataset.extract_all(ses_id='ses-s1', type='CondRun', atlas='MNISymDentate1', smooth=None)
+    mdtb_dataset.extract_all(ses_id='ses-s2', type='CondRun', atlas='MNISymDentate1', smooth=None)
 
     # smooth_mdtb_fs32k(ses_id='ses-s1', type='CondHalf', smooth=4, kernel='fwhm')
     # smooth_mdtb_fs32k(ses_id='ses-s2', type='CondHalf', smooth=4, kernel='fwhm')
