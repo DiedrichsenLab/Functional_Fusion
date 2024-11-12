d0586b1fc715d28fa877ac9138d1552c7d5ce1fd
incehusain <144962266+incehusain@users.noreply.github.com>
2024-11-12 13:40:58 -0800

    initial commit

---
+++ notebooks/decomposing_variances.py
@@ -0,0 +1,30 @@
+def flat2ndarray(flat_data, cond_vec, part_vec):
+    """
+    convert flat data (n_subjects x n_trials x n_voxels) into a 4d ndarray (n_subjects x n_partitions x n_conditions x n_voxels)
+
+    Args:
+        flat_data:
+        part_vec:
+        cond_vec:
+
+    Returns:
+        data
+
+    """
+
+    [n_subjects, n_trials, n_voxels] = flat_data.shape
+
+    unique_conditions = np.unique(cond_vec)
+    unique_partitions = np.unique(part_vec)
+
+    n_conditions = unique_conditions.size
+    n_partitions = unique_partitions.size
+   
+    data = np.zeros((n_subjects, n_partitions, n_conditions, n_voxels))
+
+    for p,partI in enumerate(unique_partitions):
+        for c,condI in enumerate(unique_conditions):
+            trial_inds = np.where(np.logical_and(cond_vec == condI, part_vec == partI))
+            data[:, p, c, :] = flat_data[:, trial_inds, :].squeeze()
+            
+    return data
