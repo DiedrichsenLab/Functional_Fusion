d6c5ed97d4a3aa41f252ffaab661eab582cf83cc
incehusain <144962266+incehusain@users.noreply.github.com>
2024-11-12 15:26:31 -0800

    stacked variance decomposition plot

--- Functional_Fusion/array_convert.py
+++ Functional_Fusion/array_convert.py
@@ -1,3 +1,7 @@
+import numpy as np
+import pandas as pd
+import nibabel as nb 
+
 def flat2ndarray(flat_data, cond_vec, part_vec):
     """
     convert flat data (n_subjects x n_trials x n_voxels) into a 4d ndarray (n_subjects x n_partitions x n_conditions x n_voxels)
@@ -28,3 +32,14 @@ def flat2ndarray(flat_data, cond_vec, part_vec):
             data[:, p, c, :] = flat_data[:, trial_inds, :].squeeze()
             
     return data
+
+def get_structure_data(structure='pontine', data_dir='/Volumes/diedrichsen_data$/data/Cerebellum/Pontine7T/RegionOfInterest_BOLDMNI/data/group'):
+    T = pd.read_csv('/Volumes/diedrichsen_data$/data/Cerebellum/Pontine7T/participants.tsv', sep='\t')
+    A = []
+    for i, good_value in zip(T.participant_id, T.good):
+        if good_value==1:
+            file_path = f'{data_dir}/beta_glm2_{structure}_{i}.dscalar.nii'
+            cifti = nb.load(file_path)
+            A.append(cifti.get_fdata())
+        to_tensor = np.array(A)
+    return to_tensor
