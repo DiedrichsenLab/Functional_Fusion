2574f30cee1d49681f6e602bfdc87f0f986d00f5
incehusain <ihusain5@uwo.ca>
2025-05-14 15:36:56 -0700

    updated thalamus plot with anatomical as bg

--- Functional_Fusion/plot.py
+++ Functional_Fusion/plot.py
@@ -247,88 +247,100 @@ def plot_thalamus2(data,
     return axes
 
 def plot_thalamus(data,
-                 bg_img=None,
-                 fig=None,
-                 gridspec=None,
-                 z_coords = [-7,-5,-3,1, 8,12,15],  
-                 cscale = [None,None],
-                 cmap = 'cold_hot',
-                 threshold = None):
-    
-    """Generate the plot for thalamus  
-    For fine control of the visulization, see https://nilearn.github.io/dev/modules/generated/nilearn.plotting.plot_img.html
+                bg_img=True,
+               # z_coords = [8],
+                z_coords = [-7,-5,-3,1, 8,12,15], 
+                cscale = [None,None],
+                cmap = 'cold_hot',
+                threshold = None,
+                fig=None,
+                gridspec = None,
+                ):
+  
+   """Generate the plot for thalamus 
+   For fine control of the visulization, see https://nilearn.github.io/dev/modules/generated/nilearn.plotting.plot_img.html
 
-    Args:
-        data (ndarray): 
-        bg_img (nifti1image): Background image. Defaults to None.
-        fig (plt.figure): pre-specified matplotlib figure. 
-        gridspec (Gridspec): A 7 x 2 Gridspec to plot the dentate data.
-        z_coords (list): Z-coordinate slice to plot. Defaults to [-7,-5,-3,1, 8,12,15].
-        cscale (list): [lower and upper] range for colorscale. None sets it to 2% percentile of data (asymmetric). 
-        cmap (str, colormap, ndarray): Name, colormap, or Nx3 ndarray. Defaults to 'cold_hot'. 
-        threshold (numeric): Single threshold: will plot data abs(y)> th
-    
-    Returns: 
-        axes (array): 7 x2 array of subplots.
-    """
-    dn,_ = am.get_atlas('MNISymThalamus1')
-    if bg_img is None:
-        adir = ut.default_atlas_dir
-        bg_img = nb.load(adir + '/tpl-MNI152NLin2009cSym/tpl-MNI152NLin2009cSym_res-1_thalamus.nii')
-    
-    # Project the functional data into the atlas space
-    fcn_img = dn.data_to_nifti(data)
 
-    # If cscale is not provided, calculate it from the data: 
-    if cscale[0] is None:
-        cscale[0] = np.percentile(data,2)
-    if cscale[1] is None:
-        cscale[1] = np.percentile(data,98)
+   Args:
+       data (ndarray):
+       bg_img (nifti1image): Background image. Defaults to None.
+       fig (plt.figure): pre-specified matplotlib figure.
+       gridspec (Gridspec): A 7 x 2 Gridspec to plot the dentate data.
+       z_coords (list): Z-coordinate slice to plot. Defaults to [-7,-5,-3,1, 8,12,15].
+       cscale (list): [lower and upper] range for colorscale. None sets it to 2% percentile of data (asymmetric).
+       cmap (str, colormap, ndarray): Name, colormap, or Nx3 ndarray. Defaults to 'cold_hot'.
+       threshold (numeric): Single threshold: will plot data abs(y)> th
+  
+   Returns:
+       axes (array): 7 x2 array of subplots.
+   """
+   dn,_ = am.get_atlas('MNISymThalamus1')
+   #if bg_img is None:
+    #   adir = ut.default_atlas_dir
+     #  bg_img = nb.load(adir + '/tpl-MNI152NLin2009cSym/tpl-MNI152NLin2009cSym_res-1_thalamus.nii')
+   if bg_img is True:
+       bg_img = nb.load('/Volumes/diedrichsen_data$/data/Cerebellum/Pontine7T/RegionOfInterest_BOLDMNI/template.nii')
+  
+   # Project the functional data into the atlas space
+   fcn_img = dn.data_to_nifti(data)
 
-    # Make a colormap from ndarray
-    if isinstance(cmap,np.ndarray):
-        cmap = ListedColormap(cmap)
 
-    # Cut out the left and right dentate at the voxel coordinates
-    c1 = np.array([[-34,-39,-10],[0,-39,-10]]).T # Lower left corner of image
-    c2 = np.array([[0,5,18],[34,5,18]]).T # Upper right corner of image
-    v1 = nt.affine_transform_mat(c1,inv(bg_img.affine)).astype(int)
-    v2 = nt.affine_transform_mat(c2,inv(bg_img.affine)).astype(int)
+   # If cscale is not provided, calculate it from the data:
+   if cscale[0] is None:
+       cscale[0] = np.percentile(data,2)
+   if cscale[1] is None:
+       cscale[1] = np.percentile(data,98)
 
-    bg = [] # Slice background data
-    fc = [] # Sliced functional data
-    for i in range(2):
-        bg.append(bg_img.slicer[v1[0,i]:v2[0,i]+1,v1[1,i]:v2[1,i]+1,v1[2,i]:v2[2,i]+1])
-        fc.append(fcn_img.slicer[v1[0,i]:v2[0,i]+1,v1[1,i]:v2[1,i]+1,v1[2,i]:v2[2,i]+1])
 
-    # Initialize the figure and axes if not provided.
-    if gridspec is None:
-        if fig is None:
-            #fig = plt.figure(figsize=(15,6),facecolor='black')
-            fig = plt.figure(figsize=(2,10),facecolor='black')
-        gridspec = fig.add_gridspec(7, 2,hspace=0.1,wspace=0.1)
-        #gridspec = fig.add_gridspec(1, 2,wspace=0.1)
-    
-    # axes
-    axes = gridspec.subplots()
+   # Make a colormap from ndarray
+   if isinstance(cmap,np.ndarray):
+       cmap = ListedColormap(cmap)
 
-    # Now use the nibabel plotting functions to plot the images
-    for i in range(2):
-        for j,z in enumerate(z_coords):
-            nlp.plot_img(fc[i],
-            display_mode="z",
-            cut_coords=[z],
-            bg_img=bg[i],
-            black_bg=True,
-            axes=axes[j,i],
-            #axes = axes[i],
-            threshold=threshold,
-            vmin=cscale[0],
-            vmax=cscale[1],
-            cmap=cmap,
-            annotate=False)
 
-    return axes
+   # Cut out the left and right dentate at the voxel coordinates
+   c1 = np.array([[-34,-39,-10],[0,-39,-10]]).T # Lower left corner of image
+   c2 = np.array([[0,5,18],[34,5,18]]).T # Upper right corner of image
+   v1 = nt.affine_transform_mat(c1,inv(bg_img.affine)).astype(int)
+   v2 = nt.affine_transform_mat(c2,inv(bg_img.affine)).astype(int)
+
+
+   bg = [] # Slice background data
+   fc = [] # Sliced functional data
+
+
+   for i in range(2):
+       bg.append(bg_img.slicer[v1[0,i]:v2[0,i]+1,v1[1,i]:v2[1,i]+1,v1[2,i]:v2[2,i]+1])
+       fc.append(fcn_img.slicer[v1[0,i]:v2[0,i]+1,v1[1,i]:v2[1,i]+1,v1[2,i]:v2[2,i]+1])
+
+
+   # Initialize the figure and axes if not provided.
+   if gridspec is None:
+       if fig is None:
+             fig = plt.figure(figsize=(2,8),facecolor='none')
+       gridspec = fig.add_gridspec(7, 2,hspace=0.1,wspace=0)
+  
+   #axes
+   axes = gridspec.subplots()
+
+
+   # Now use the nibabel plotting functions to plot the images
+   for i in range(2):
+       for j,z in enumerate(z_coords):
+           nlp.plot_img(fc[i],
+           display_mode="z",
+           cut_coords=[z],
+           bg_img=bg_img,
+           black_bg=True,
+           axes=axes[j,i],
+           threshold=0.1,
+           vmin=cscale[0],
+           vmax=cscale[1],
+           cmap=cmap,
+           annotate=False,
+           )
+
+
+   return axes
 
 def plot_olive2(data,
                  bg_img=None,
