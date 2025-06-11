78e1827a04b93d86753a049aa80924c6f574ff21
Bassel Arafat <118376927+Barafat2@users.noreply.github.com>
2025-06-11 17:57:25 -0400

    extract language

--- scripts/extract_language_task.py
+++ scripts/extract_language_task.py
@@ -8,45 +8,33 @@ Author: Bassel Arafat
 import pandas as pd
 from pathlib import Path
 # import mat73
-import Functional_Fusion.atlas_map as am
 from Functional_Fusion.dataset import DataSetLanguage
 
 
-base_dir = '/Volumes/diedrichsen_data$/data/FunctionalFusion'
+base_dir = '/Volumes/diedrichsen_data$/data/FunctionalFusion_new'
 if not Path(base_dir).exists():
-    base_dir = '/cifs/diedrichsen/data/FunctionalFusion'
+    base_dir = '/cifs/diedrichsen/data/FunctionalFusion_new'
 if not Path(base_dir).exists():
-    base_dir = 'Y:/data/FunctionalFusion'
+    base_dir = 'Y:/data/FunctionalFusion_new'
 
 
 data_dir = base_dir + '/Language'
 atlas_dir = base_dir + '/Atlases'
 
 
-types = ['CondRun']
-atlases  = ['MNISymThalamus1']
-session_list = ['ses-localizer_cond_fm']
+types = ['CondRun','CondAll','CondHalf']
+atlases  = ['fs32k','MNISymC3']
+session_list = ['ses-localizer','ses-localizerfm']
 
+subj = 'all'
+subj = [6]
 
 LL_dataset = DataSetLanguage(data_dir)
 for ses in session_list:
-
     print(f'extracting session {ses}')
-    participants_tsv = pd.read_csv(f'{data_dir}/participants.tsv',sep = '\t')
-    if ses  == 'ses-localizer_cond' or ses == 'ses-localizer_cond_fm':
-        filtered_participants = participants_tsv[participants_tsv['ses'].str.contains('loc')]
-        subj_list = filtered_participants['participant_id'].tolist()
-    elif ses == 'ses-sencoding_category_fixed' or ses == 'ses-sencoding_category_duration' or ses == 'ses-sencoding_trial_fixed' or ses == 'ses-sencoding_trial_duration' :
-        filtered_participants = participants_tsv[participants_tsv['ses'].str.contains('sen')]
-        subj_list = filtered_participants['participant_id'].tolist()
-
-    else:
-        raise Exception('wrong session values')
-    
-    
     for type in types:
         print(f'extracting type {type}')
         for atlas in atlases:
             print(f'extracting atlas: {atlas}')
-            LL_dataset.extract_all(ses_id = ses,type = type, atlas = atlas, smooth=None, subj='all')
+            LL_dataset.extract_all(ses_id = ses,type = type, atlas = atlas, smooth=None, subj=subj)
  
