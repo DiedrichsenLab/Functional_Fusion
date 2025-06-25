6eb02fccd59dfdd8d04040b482e57f66956e90da
Caro Nettekoven <30801389+carobellum@users.noreply.github.com>
2025-06-25 14:23:11 +0200

    Update extract_all.py

--- scripts/extract_all.py
+++ scripts/extract_all.py
@@ -28,22 +28,13 @@ def extract_dataset(dataset,space,type):
             interpolation = 1 
         for t in type:
             for sess in mydataset.sessions:
-                if sess !='ses-rest':
+                if sess =='ses-rest':
                     print(f'extracting {dataset} type {t} space {sp}')
                     mydataset.extract_all(ses_id=sess, type=t, atlas=sp,smooth=smooth,interpolation=interpolation)
 
 
 if __name__ == "__main__":
-    # datasets = ['WMFS','MDTB','Nishimoto','Somatotopic','IBC']
-    # extract_dataset('MDTB', ['fs32k','MNISymC3'], ['CondAll','CondRun'])
-    # extract_dataset('MDTB', ['fs32k','MNISymC3'], ['CondHalf'])
-    #extract_dataset('WMFS', ['fs32k','MNISymC3'], ['CondHalf'])
-    extract_dataset('HCPur100', ['fs32k','MNISymC3'], ['CondAll','CondRun'])
-    # extract_dataset('Nishimoto', ['fs32k','MNISymC3'], ['CondHalf'])
-    # extract_dataset('Somatotopic', ['fs32k','MNISymC3'], ['CondHalf'])
-    # extract_dataset('WMFS', ['MNISymC3'], ['CondHalf','CondAll','CondRun'])
-    # extract_dataset('WMFS', ['MNISymC3'], ['CondHalf','CondAll','CondRun'])
-    # extract_dataset('WMFS', ['MNISymC3'], ['CondHalf','CondAll','CondRun'])
-    # extract_dataset('WMFS', ['MNISymC3'], ['CondHalf','CondAll','CondRun'])
-    # extract_dataset('WMFS', ['MNISymC3'], ['CondHalf','CondAll','CondRun'])
-    # ['Social','Language','WMFS','MDTB','Demand','Nishimoto','Somatotopic','IBC']
+    
+    extract_dataset('Social', ['fs32k','MNISymC3'], ['CondRun'])
+    extract_dataset('Language', ['fs32k','MNISymC3'], ['CondRun'])
+    extract_dataset('MDTB', ['fs32k','MNISymC3'], ['CondRun'])
