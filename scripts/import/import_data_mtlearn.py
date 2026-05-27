56ca7f66cb065f5d85a327392180858059aec67b
Jorn Diedrichsen <joern.diedrichsen@googlemail.com>
2026-05-27 15:21:58 -0400

    import

--- scripts/import/import_data_mtlearn.py
+++ scripts/import/import_data_mtlearn.py
@@ -5,11 +5,11 @@ import Functional_Fusion.import_data as id
 import scripts.fusion_paths as paths
 import shutil
 from pathlib import Path 
-
+import nibabel as nb
 
 base_dir = paths.set_base_dir()
 
-orig_dir = base_dir + '/Cerebellum/LearningMultiTask_Maedbh'
+orig_dir = base_dir + 'Cerebellum/LearningMultiTask_Maedbh'
 target_dir = base_dir + 'FunctionalFusion_new/MTLearn'
 
 def safe_copy(src,dest):
@@ -34,20 +34,56 @@ def import_surface():
         dest = target_dir + f'/derivatives/ffimport/{sub}/anat'
         id.import_freesurfer(src, dest, sub, sub)
 
-def import_mean_bold():
+def import_bold():
     T = pd.read_csv(target_dir + '/participants.tsv', delimiter='\t')
+    D = pd.read_csv(target_dir + '/reginfo.tsv',delimiter='\t')
     participants = T.participant_id
-    for s in participants:
-        subject_orig = s.replace('sub-', 's', 1)
-        src = base_dir + f'/Cerebellum/super_cerebellum/sc1/imaging_data/{subject_orig}/rmeanepi.nii'
-        dest = base_dir + f'/FunctionalFusion/MDTB/derivatives/{s}/anat/{s}_meanbold.nii'
-        shutil.copyfile(src,dest)
+    for sub in participants:
+        for ses in ['ses-1','ses-2','ses-3']:
+            subj_dir = target_dir + f'/derivatives/ffimport/{sub}/func/{ses}'
+            Path(subj_dir).mkdir(parents=True, exist_ok=True)
+
+            # Average all _rmse.nii files across runs
+            X = []
+            for run in range(1, 7):
+                rmse_file = nb.load(orig_dir + f'/func/{sub}/{sub}_glm-02_{ses}_run-{run:02d}_rmse.nii')
+                X.append(rmse_file.get_fdata())
+            mean_rmse = np.stack(X, axis=3).mean(axis=3)
+            mean_file = nb.Nifti1Image(mean_rmse, rmse_file.affine, rmse_file.header)
+            mean_file.to_filename(subj_dir + f'/{sub}_{ses}_resms.nii')
+
+            # Save mask 
+            X = mean_rmse >0 
+            mask_file = nb.Nifti1Image(X.astype(np.uint8), rmse_file.affine)
+            mask_file.to_filename(subj_dir + f'/{sub}_{ses}_mask.nii')
+
+            # Break up the 4d niftis into 3d niftis for each run and save them as _run-xx_reg-xx.nii files
+            reginfo=[]
+            for run in range(1, 7):
+                beta_file = nb.load(orig_dir + f'/func/{sub}/{sub}_glm-02_{ses}_run-{run:02d}_betas.nii')
+                X = beta_file.get_fdata()
+                if X.shape[3] != 16:
+                    print(f'Warning: expected 16 regressors but found {X.shape[3]} for {sub} {ses} run {run}')
+                for reg in range(16):
+                    reg_file = nb.Nifti1Image(X[:,:,:,reg], beta_file.affine, beta_file.header)
+                    reg_file.to_filename(subj_dir + f'/{sub}_{ses}_run-{run:02d}_reg-{reg:02d}.nii')
+                d = pd.DataFrame({'run': [run]*16, 'task_name': D['task_name'], 
+                                  'cond_name': D['cond_name'], 
+                                  'reg_id': D['reg_id'],    
+                                  'instruction': D['instruction'], 
+                                  'task_code': D['task_code'], 
+                                  'cond_code': D['cond_code']
+                                  })
+                reginfo.append(d)
+            reginfo = pd.concat(reginfo, ignore_index=True)
+            reginfo.to_csv(subj_dir + f'/{sub}_{ses}_reginfo.tsv', sep='\t', index=False)
+            
 
 if __name__ == "__main__":
     # fix_sc2_reginfo()
     # import_anatomical()
-    import_surface() 
-
+    # import_surface() 
+    import_bold()   
     # for s in participants:
     # old_id = s.replace('sub-','s',1)
     # dir1 = orig_dir + f'/sc1/suit/anatomicals/{old_id}'
